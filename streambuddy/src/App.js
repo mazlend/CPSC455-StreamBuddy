2b6aa58807ddc6515d7ef4d56a72566b3536c087
saadishahid <39744515+saadishahid@users.noreply.github.com>
2021-06-11 16:24:30 -0700

    added navbar to the render

--- streambuddy/src/App.js
+++ streambuddy/src/App.js
@@ -3,17 +3,31 @@ import './App.css';
 
 import MoviecardList from "./components/MoviecardList";
 import SearchBar from "./components/SearchBar";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import {Navbar} from "./components/Navbar";
+
+
+
 
 function App() {
   return (
       <div className="App">
+          <BrowserRouter>
+              <Navbar />
+              <Switch>
+                  <Route exact path="/" component={() => <div>Home</div>} />
+                  <Route path="/Login" component={() => <div>Login</div>} />
+                  <Route path="/Signup" component={() => <div>Sign up</div>} />
+              </Switch>
+          </BrowserRouter>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+
           <p>
             <SearchBar />
           </p>
           <MoviecardList />
-        </header>
+       </header>
       </div>
   );
 }
