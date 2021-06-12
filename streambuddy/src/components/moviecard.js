5de24a8a51fbac0520eefce126564b59ecfeb870
saadishahid <39744515+saadishahid@users.noreply.github.com>
2021-06-12 09:35:20 -0700

    added hover effect for See Details

--- streambuddy/src/components/moviecard.js
+++ streambuddy/src/components/moviecard.js
@@ -7,19 +7,41 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Popover from '@material-ui/core/Popover';
 
 const useStyles = makeStyles({
     root: {
-        maxWidth: 300,
+        maxWidth: 345,
     },
     media: {
-        height: 300,
+        height: 345,
+    },
+    popover: {
+        pointerEvents: 'none',
+    },
+    paper: {
+        padding: 3,
     },
 });
 
+
+
 export default function Moviecard(props) {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
+    const handlePopoverOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+
+
+    // For the Popover we used parts of the demo code of the material-ui documentation: https://codesandbox.io/s/f9n6j?file=/demo.js:922-1436
     return (
         <Card className={classes.root}>
             <CardActionArea>
@@ -29,7 +51,7 @@ export default function Moviecard(props) {
                     title={props.item.moviename}
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h6" component="h2">
+                    <Typography gutterBottom variant="h5" component="h2">
                         {props.item.moviename}
                     </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
@@ -43,9 +65,39 @@ export default function Moviecard(props) {
                 <Button size="small" color="primary">
                     Watch on Netflix
                 </Button>
-                <Button size="small" color="primary">
-                    Full Description
-                </Button>
+                <Typography
+                    aria-owns={open ? 'mouse-over-popover' : undefined}
+                    aria-haspopup="true"
+                    onMouseEnter={handlePopoverOpen}
+                    onMouseLeave={handlePopoverClose}>
+                    SEE FULL DESCRIPTION
+                </Typography>
+                <Popover
+                    id="mouse-over-popover"
+                    className={classes.popover}
+                    classes={{
+                        paper: classes.paper,
+                    }}
+                    open={open}
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    onClose={handlePopoverClose}
+                    disableRestoreFocus
+                >
+                    <Typography>
+                        <p> {props.item.moviename}</p>
+                        <p> Description of the movie: Some description of the movie, maybe a trailer </p>
+                        <p> Some additional info </p>
+                        <p> {props.item.moviereleaseyear}</p>
+                    </Typography>
+                </Popover>
             </CardActions>
         </Card>
     );
