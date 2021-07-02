4cf8b29eb77a2b4f21821ccc030af1d2f75f0bdb
FlorentinaSimlinger <60526092+FlorentinaSimlinger@users.noreply.github.com>
2021-07-02 07:58:19 +0100

    add basic user profile

--- streambuddy/src/App.js
+++ streambuddy/src/App.js
@@ -1,40 +1,47 @@
-
 import MoviecardList from "./components/MoviecardList";
 import SearchBar from "./components/SearchBar";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import {Navbar} from "./components/Navbar";
-
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import Profile from "./components/Profile";
 import theme from "./components/Theme";
-import {ThemeProvider} from "@material-ui/styles";
-
+import { ThemeProvider } from "@material-ui/styles";
 import SearchButton from "./components/SearchButton";
-
-
 import PersistentDrawerLeft from "./components/newSidebar";
 
-
-
 function App() {
-    return (
-          <div className="app">
-            
-            <div className="navbar">
-                <ThemeProvider theme={theme}>
-                    <BrowserRouter>
-                        <Navbar />
-                        <PersistentDrawerLeft />
-                        <Switch>
-                            <Route exact path="/" />
-                            <Route path="/Login" />
-                            <Route path="/Signup" />
-                        </Switch>
-                    </BrowserRouter>
-                </ThemeProvider>
-            </div>
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <ThemeProvider theme={theme}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/profile">ThisLinkNeedsToBeInLogin</Link>
+          </li>
+          <Switch>
+            <Route exact path="/" />
+            <Route path="/Login" />
+            <Route path="/Signup" />
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </ThemeProvider>
+      </div>
+    </BrowserRouter>
+  );
+}
 
-          </div>
-
-    );
+{
+  /* <Navbar />
+<PersistentDrawerLeft /> */
 }
 
 export default App;
---
+++ streambuddy/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CommentInput from './CommentInput';
+
+
+function Profile() {
+
+    return(
+        <div>
+           <p> User profile </p>
+           <CommentInput/>
+        </div>
+    );
+}
+
+export default Profile;
