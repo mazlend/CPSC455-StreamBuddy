9e172a20b2e026390eff12b50eca9317082533a6
naztazeem <nazish.tazeem@gmail.com>
2021-07-02 17:57:18 -0700

    created Film and User schemas

--- backend/models/Film.js
+++ backend/models/Film.js
@@ -2,5 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const filmSchema = new Schema({
+    Title: { type: String, require: true },
+    Year: { type: Number, min: 1900, max: 2025 },
+    Rated: { type: String },
+    Released: {type: Date },
+    Runtime: { type: String},
+    Genre: { type: String},
+    Director: { type: String },
+    Writer: { type: String },
+    Plot: { type: String, required: true },
+    Language: { type: String , required: true },
+    Country: { type: String, required: true },
+    Awards: { type: String },
+    Poster: { type: String },
+    Ratings: [
+        {
+            Source: { type: String },
+            Value: { type: Schema.Types.Decimal128 }
+        },
+        {
+            Source: { type: String },
+            Value: { type: Schema.Types.Decimal128 }
+        },
+        {
+            Source: { type: String },
+            Value: { type: Schema.Types.Decimal128 }
+        }
+        ],
+    imdbRating: { type: Schema.Types.Decimal128 },
+    imdbVotes: { type: Number },
+    imdbId: { type: String },
+    BoxOffice: { type: Number },
+    Production: { type: String },
 
-})
+});
+
+module.exports = mongoose.model('Film', filmSchema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -6,4 +6,4 @@ const userSchema = new Schema({
 
 });
 
-module.exports = mongoose.model('User, userSchema');
+module.exports = mongoose.model('User', userSchema);
