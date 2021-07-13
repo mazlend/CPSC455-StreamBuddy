dd1ab465d9cee13839173296a62c24f581430646
naztazeem <nazish.tazeem@gmail.com>
2021-07-13 01:08:43 -0700

    getUser and createUser

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new Schema({
     googleId: {type: String, required: true},
     imageUrl: {type: String },
     email: { type: String, required: true, unique: true },
-    displayName: { type: String, required: true },
+    name: { type: String, required: true },
     lastName: { type: String },
     firstName: { type: String }
 
 });
+
+userSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('User', userSchema);
