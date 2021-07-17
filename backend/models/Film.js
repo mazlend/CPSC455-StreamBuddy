1cc5a16fb69397046656e268e31d66c8857561c5
naztazeem <nazish.tazeem@gmail.com>
2021-07-16 23:55:09 -0700

    updated film schema

--- backend/models/Film.js
+++ backend/models/Film.js
@@ -1,40 +1,42 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const filmSchema = new Schema({
-    Title: { type: String, require: true },
-    Year: { type: Number, min: 1900, max: 2025 },
-    Rated: { type: String },
-    Released: {type: Date },
+    Title: { type: String, required: true},
+    Year: { type: String },
+    Rated: { type: String},
+    Released: {type: String },
     Runtime: { type: String},
     Genre: { type: String},
     Director: { type: String },
     Writer: { type: String },
-    Plot: { type: String, required: true },
-    Language: { type: String , required: true },
-    Country: { type: String, required: true },
+    Actors: {type: String},
+    Plot: { type: String, required: true},
+    Language: { type: String, required:true},
+    Country: { type: String, required:true },
     Awards: { type: String },
     Poster: { type: String },
     Ratings: [
         {
             Source: { type: String },
-            Value: { type: Schema.Types.Decimal128 }
+            Value: {type: String  }
         },
         {
             Source: { type: String },
-            Value: { type: Schema.Types.Decimal128 }
+            Value: {type: String }
         },
         {
             Source: { type: String },
-            Value: { type: Schema.Types.Decimal128 }
+            Value: { type: String}
         }
-        ],
-    imdbRating: { type: Schema.Types.Decimal128 },
-    imdbVotes: { type: Number },
+    ],
+    imdbRating: {  },
+    imdbVotes: {  },
     imdbId: { type: String },
-    BoxOffice: { type: Number },
+    Type: {type:String},
+    BoxOffice: {  },
     Production: { type: String },
-
 
 });
 
