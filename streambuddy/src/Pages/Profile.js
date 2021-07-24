5b7cdb9e7fb93e9b44c886a1fb5d7064fa62683d
FlorentinaSimlinger <60526092+FlorentinaSimlinger@users.noreply.github.com>
2021-07-24 17:25:16 +0100

    add user props

--- streambuddy/src/Pages/Profile.js
+++ streambuddy/src/Pages/Profile.js
@@ -5,55 +5,26 @@ import Reviews from '../components/Reviews';
 import Friends from '../components/Friends';
 import User from '../components/User';
 import {Container} from "@material-ui/core";
-import axios from "axios";
-import {watchlist} from "../watchlist";
-import {watchedMovies} from "../watchedMovies";
 
+function Profile(props) {
 
-
-function Profile() {
-
-    // we use denseView to determine whether render the expanded version with the movie posters or a dense list-like version
+    // TODO: we use denseView to determine whether render the expanded version with the movie posters or a dense list-like version
     // we want to have the option for a dense view as users may not want to see every category with posters
+    // not yet fully implemented
     const [views, setViews] = React.useState(
         {
             denseViewWatchList: false,
             denseViewWatchedMovies: false
         }
     )
-
-    const [user, setUser] = React.useState(null);
-
-    // TODO: use a working google id, as a next step we need to somehow pass the google id from authetication to here
-    const googleId = "968372237624-r36vh877rae0tkteofmqtap4mtbm9sgh.apps.googleusercontent.com"
-
-    const tempUserName = "popcornFan"
-    const tempWatchList = watchlist;
-    const tempWatchedMovies = watchedMovies;
-
-    const getUser = () => {
-        axios.get(`http://localhost:5000/api/users/${googleId}`)
-            .then((res) => {
-                if (res.data) {
-                    setUser(res.data)
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-
-    React.useEffect(() => {
-        getUser();
-    }, []);
     
     return(
         <Container maxWidth="lg">
             <div>
-                {user || tempUserName && <User name={tempUserName}/>}
+                {user && <User name={props.user.name}/>}
                 <ProfileNavbar />
-                {user || tempWatchList && <MoviecardListWrapper id="watchlist" name="Watchlist" movieList={tempWatchList} denseView={views.denseViewWatchList}/> }
-                {user || tempWatchedMovies && <MoviecardListWrapper id="watchedMovies" name="Watched Movies" movieList={tempWatchedMovies} denseView={views.denseViewWatchedMovies} /> }
+                {user && <MoviecardListWrapper id="watchlist" name="Watchlist" movieList={props.user.watchlist} denseView={views.denseViewWatchList}/> }
+                {user && <MoviecardListWrapper id="watchedMovies" name="Watched Movies" movieList={props.user.watched} denseView={views.denseViewWatchedMovies} /> }
                 <Reviews id="reviews" />
                 <Friends id="friends" />
             </div>
