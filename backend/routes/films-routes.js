e82edfac65377e5d5a5d7a11c219c8f24d9a123c
UBC Student <47469240+jkwok2@users.noreply.github.com>
2021-07-27 13:12:11 -0700

    country advanced search

--- backend/routes/films-routes.js
+++ backend/routes/films-routes.js
@@ -6,6 +6,7 @@ const Film = mongoose.model('Film', filmSchema)
 
 router.get('/:id', async function (req, res, next) {
     // res.status(200).json({ message: 'GET Films is working'});
+    console.log(req.params);
     let filmName = req.params.id.replace("_", " ");
     try {
         let filmData = await Film.find({Title: filmName});
@@ -14,6 +15,22 @@ router.get('/:id', async function (req, res, next) {
         res.send(err);
     }
 });
+
+router.get('/advanced/foo', async function (req, res, next) {
+    console.log(req.body);
+    let queryCountry = req.body.country;
+    // console.log(queryCountry);
+    let queryGenre = req.body.genre;
+    console.log(queryGenre);
+    let results = new Set();
+    try {
+        let filmDataCountry = await Film.find({Country: queryCountry});
+        // console.log(filmDataCountry.length);
+        res.send(filmDataCountry);
+    } catch (err) {
+        res.send(err);
+    }
+ });
 
 router.get('/', async function (req, res, next) {
     res.status(200).json({ message: 'GET Films is working'});
