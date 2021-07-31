8b6d3012da0d335e74c107b9744ed3131adde765
saadishahid <39744515+saadishahid@users.noreply.github.com>
2021-07-30 17:37:33 -0700

    whatever

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//const cors = require('cors');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     next();
 });
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/films', filmsRoutes);
