67c6bc1378539bf652abfec04a2b920eb5e8c6d8
naztazeem <nazish.tazeem@gmail.com>
2021-08-12 00:21:16 -0700

    debugged developer tool error for networkbar

--- streambuddy/src/components/NetworkBar.js
+++ streambuddy/src/components/NetworkBar.js
@@ -17,18 +17,25 @@ const useStyles = makeStyles((theme) => ({
 
 export default function NetworkBar() {
     const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <Paper square className={classes.root}>
             <Tabs
                 variant="fullWidth"
                 textColor="black"
+                value={value}
+                onChange={handleChange}
             >
-                <Tab className={classes.tab} label="Name"  style={{ minWidth: "32%" }} />
-                <Tab className={classes.tab} label="Seen"  style={{ minWidth: "18%" }} />
-                <Tab className={classes.tab} label="Watchlist"  style={{ minWidth: "8%" }} />
-                <Tab className={classes.tab} label="Reviews"  style={{ minWidth: "15%" }} />
-                <Tab className={classes.tab}  style={{ minWidth: "22%" }} />
+                <Tab className={classes.tab} label="Name"  style={{ minWidth: "32%" }} disabled/>
+                <Tab className={classes.tab} label="Seen"  style={{ minWidth: "18%" }} disabled/>
+                <Tab className={classes.tab} label="Watchlist"  style={{ minWidth: "8%" }} disabled/>
+                <Tab className={classes.tab} label="Reviews"  style={{ minWidth: "15%" }} disabled/>
+                <Tab className={classes.tab}  style={{ minWidth: "22%" }} disabled/>
             </Tabs>
         </Paper>
 
